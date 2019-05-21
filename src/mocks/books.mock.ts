2edeb9c99c84d6055da887a94718b9120d076368
juliendcode <juliendcode@gmail.com>
2019-05-21 10:57:53 +0200

    book module

---
+++ src/mocks/books.mock.ts
@@ -0,0 +1,39 @@
+export const BOOKS = [
+  {
+    id: 1,
+    title: 'Node.js',
+    description: 'Des applications ultra rapides avec Node.js',
+    author: 'Mathieu Nebra',
+  },
+  {
+    id: 2,
+    title: 'Logicomix',
+    description:
+      'Logicomix est un roman graphique sur la quête des fondements des mathématiques.',
+    author: 'Apóstolos Doxiádis',
+  },
+  {
+    id: 3,
+    title: 'Third book',
+    description: 'This is the description for the third book',
+    author: 'Clement Wilfred',
+  },
+  {
+    id: 4,
+    title: 'Fourth book',
+    description: 'This is the description for the fourth book',
+    author: 'Christian nwamba',
+  },
+  {
+    id: 5,
+    title: 'Fifth book',
+    description: 'This is the description for the fifth book',
+    author: 'Chris anderson',
+  },
+  {
+    id: 6,
+    title: 'Sixth book',
+    description: 'This is the description for the sixth book',
+    author: 'Olususi Oluyemi',
+  },
+];
